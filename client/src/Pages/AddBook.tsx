adfecaafcdeda3d2ba117e2431ff4fe7afe74a66
pranabcode2023 <pranabcode2023@gmail.com>
2023-05-20 13:14:28 +0200

    some changes

--- client/src/Pages/AddBook.tsx
+++ client/src/Pages/AddBook.tsx
@@ -1,53 +1,44 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { ChangeEvent } from 'react';
-
 
 const AddBook = () => {
   const history = useNavigate();
   
   const [inputs, setInputs] = useState({
-    name: '',
-    description: '',
-    price: '',
-    author: '',
+    name: "",
+    description: "",
+    price: "",
+    author: "",
     available: false,
-    image: '',
+    image: ""
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputs((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value,
     }));
   };
 
-  // const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setInputs((prevState) => ({
-  //     ...prevState,
-  //     [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value,
-  //   }));
-  // };
-
-
-  //   const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
-  //   if (e.target.files) {
-  //     setInputs({ ...inputs, image: e.target.files[0] })
-  //   } else {
-  //     setInputs({ ...inputs, image: "" })
-  //   }
-  // }
+  const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      setInputs({ ...inputs, image: URL.createObjectURL(file) });
+    } else {
+      setInputs({ ...inputs, image: "" });
+    }
+  };
   
   const sendRequest = async () => {
     try {
       await axios.post(`${process.env.REACT_APP_BASE_URL}books/all`, {
-        name: String(inputs.name),
-        author: String(inputs.author),
-        description: String(inputs.description),
+        name: inputs.name,
+        author: inputs.author,
+        description: inputs.description,
         price: Number(inputs.price),
-        image: String(inputs.image),
-        available: Boolean(inputs.available),
+        image: inputs.image,
+        available: inputs.available,
       });
     } catch (error) {
       console.error(error);
@@ -56,12 +47,11 @@ const AddBook = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // submit logic here
     sendRequest().then(() => history('/books'));
   };
 
   return (
-    <div className="register">
+    <div className="addBook">
       <form className="form" onSubmit={handleSubmit}>
         <div className="title">Add Book</div>
 
@@ -84,7 +74,7 @@ const AddBook = () => {
 
         <div className="input-container">
           <label htmlFor="image">Image</label>
-          <input type="text" id="image" placeholder="Image" value={inputs.image} onChange={handleChange} name="image" />
+          <input type="file" id="image" placeholder="Image" onChange={handleFile} name="image" />
         </div>
 
         <div className="input-container">
@@ -102,4 +92,138 @@ const AddBook = () => {
 
 export default AddBook;
 
+// import React, { ChangeEvent, FormEvent, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
 
+// type SubmitBookData = {
+//   name: string;
+//   description: string;
+//   price: number;
+//   author: string;
+//   available: boolean;
+//   image: string;
+// };
+
+// const AddBook = () => {
+//   const navigate = useNavigate();
+
+//   const [formData, setFormData] = useState<SubmitBookData>({
+//     name: '',
+//     description: '',
+//     price: 0,
+//     author: '',
+//     available: false,
+//     image: '',
+//   });
+
+//   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value });
+//   };
+
+//   const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
+//     if (e.target.files) {
+//       setFormData({ ...formData, image: URL.createObjectURL(e.target.files[0]) });
+//     } else {
+//       setFormData({ ...formData, image: '' });
+//     }
+//   };
+
+//   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     console.log(formData);
+
+//     const submitData = new FormData();
+//     submitData.append('name', formData.name);
+//     submitData.append('description', formData.description);
+//     submitData.append('price', formData.price.toString());
+//     submitData.append('author', formData.author);
+//     submitData.append('available', String(formData.available));
+//     submitData.append('image', formData.image);
+
+//     navigate('/books'); // navigate to books page
+
+//     const requestOptions = {
+//       method: 'POST',
+//       body: submitData,
+//     };
+//     try {
+//       const response = await fetch(`${process.env.REACT_APP_BASE_URL}books/all`, requestOptions);
+//       const result = await response.json();
+//       console.log(result);
+//       alert('Success! Check console.');
+//     } catch (error) {
+//       console.log(error);
+//       alert('Something went wrong - check console.');
+//     }
+//   };
+
+//   return (
+//     <div className="register">
+//       <form className="form" onSubmit={handleSubmit}>
+//         <div className="title">Add Book</div>
+
+//         <div className="input-container">
+//           <input
+//             type="text"
+//             id="name"
+//             placeholder="Name"
+//             value={formData.name}
+//             onChange={handleChange}
+//             name="name"
+//           />
+//         </div>
+//         <div className="input-container">
+//           <input
+//             type="text"
+//             id="author"
+//             placeholder="Author"
+//             value={formData.author}
+//             onChange={handleChange}
+//             name="author"
+//           />
+//         </div>
+//         <div className="input-container">
+//           <input
+//             type="text"
+//             id="description"
+//             placeholder="Description"
+//             value={formData.description}
+//             onChange={handleChange}
+//             name="description"
+//           />
+//         </div>
+//         <div className="input-container">
+//           <input
+//             type="text"
+//             id="price"
+//             placeholder="Price"
+//             value={formData.price.toString()}
+//             onChange={handleChange}
+//             name="price"
+//           />
+//         </div>
+
+//         <div className="input-container">
+//           <input type="file" id="image" placeholder="Image" onChange={handleFile} name="image" />
+//         </div>
+
+//         <div className="input-container">
+//           <label>Available</label>
+//           <input
+//             type="checkbox"
+//             id="available"
+//             checked={formData.available}
+//             onChange={handleChange}
+//             name="available"
+//           />
+//         </div>
+
+//         <div className="button-container">
+//           <button type="submit">Add Book</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AddBook;
--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -39,12 +39,14 @@ const getById = async (req, res) => {
 }
 
 const addBook = async (req, res) => {
-
+    // //connectin to cloudinary
     // const bookImageUpload = await imageUpload(req.file, "user_books");
     // const newBook = new BooksModel({
     //     ...req.body,
     //     bookImageUpload: bookImageUpload
     // });
+
+    // //connectin to cloudinary
 
     let book;
     const { name, author, description, price, available, image } = req.body;
@@ -110,3 +112,89 @@ const deleteBook = async (req, res) => {
 
 
 export { getAllbooks, addBook, getById, updateBook, deleteBook };
+
+
+
+// import BooksModel from "../models/booksModel.js";
+// import { imageUpload } from "../utils/imageManagement.js";
+
+// const getAllbooks = async (req, res) => {
+//     try {
+//         const books = await BooksModel.find();
+//         res.status(200).json({ books });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ error: "Something went wrong..." });
+//     }
+// };
+
+// const getById = async (req, res) => {
+//     const id = req.params.id;
+//     try {
+//         const book = await BooksModel.findById(id);
+//         if (!book) {
+//             return res.status(404).json({ message: "No Book Found" });
+//         }
+//         res.status(200).json({ book });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ error: "Something went wrong..." });
+//     }
+// };
+
+// const addBook = async (req, res) => {
+//     try {
+//         const userBooks = await imageUpload(req.file, "user_books");
+//         const newBook = new BooksModel({
+//             ...req.body,
+//             userBooks: userBooks,
+//         });
+//         const savedBook = await newBook.save();
+//         res.status(201).json({ book: savedBook });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ message: "Unable to add the book" });
+//     }
+// };
+// const updateBook = async (req, res) => {
+//     const id = req.params.id;
+//     const { name, author, description, price, available, image } = req.body;
+//     try {
+//         const updatedBook = await BooksModel.findByIdAndUpdate(
+//             id,
+//             {
+//                 name,
+//                 author,
+//                 description,
+//                 price,
+//                 available,
+//                 image,
+//             },
+//             { new: true }
+//         );
+//         if (!updatedBook) {
+//             return res.status(404).json({ message: "Unable to update by this ID" });
+//         }
+//         res.status(200).json({ book: updatedBook });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ message: "Something went wrong..." });
+//     }
+// };
+
+// const deleteBook = async (req, res) => {
+//     const id = req.params.id;
+//     try {
+//         const deletedBook = await BooksModel.findByIdAndRemove(id);
+//         if (!deletedBook) {
+//             return res.status(404).json({ message: "Unable to delete by this ID" });
+//         }
+//         res.status(200).json({ message: "Successfully Deleted" });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ message: "Something went wrong..." });
+//     }
+// };
+
+// export { getAllbooks, addBook, getById, updateBook, deleteBook };
+
