0f6d0395f91c806cfd22ad9cafd7ba77c799ecf9
pranabcode2023 <pranabcode2023@gmail.com>
2023-05-30 23:46:06 +0200

    some changes

--- client/src/components/Profile/DeleteProfile.tsx
+++ client/src/components/Profile/DeleteProfile.tsx
@@ -1,69 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// interface ProfileProps {
-//   profile: {
-//   _id: any,
- 
-//   email: any,
-//   username: any,
-//   books: any,
-
-//   image: any
-//   };
-// }
-
-// const Profile: React.FC<ProfileProps > = ({ profile}) => {
-//   const { _id, email, username, books, image } = profile;
-
-//   const deleteHandler = async () => {
-//     try {
-//       const response = await fetch(
-//         `${process.env.REACT_APP_BASE_URL}authors/delete/${_id}`,
-//         {
-//           method: 'DELETE',
-//         }
-//       );
-
-//       if (response.ok) {
-//         // Refresh the page after deleting the item
-//         window.location.reload();
-//       } else {
-//         console.log('Error deleting profile:', response.status);
-//       }
-//     } catch (error) {
-//       console.log('Error:', error);
-//     }
-//   };
-
-//   return (
-//     <div className="bookCard">
-//       <div className="container">
-//         <img src={image} alt={image} />
-//       </div>
-//       <div className="container">
-//         <h3> {email}</h3>
-//         {/* <h3>{name}</h3> */}
-//         <h3>{username}</h3>
-//         <h3>{books}</h3>
-//        {/* <h3>{bio}</h3> */}
-//       </div>
-
-//       <div className="container">
-//         <Link to={`/authors/${_id}`}>
-//           <button>Update</button>
-//         </Link>
-//       </div>
-
-//       <div className="container">
-//         <button onClick={deleteHandler}>Delete</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Profile;
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -80,27 +14,27 @@ interface ProfileProps {
 }
 
 const DeleteProfile: React.FC<ProfileProps> = ({ profile }) => {
-  // const { _id, email, username, books, image } = profile;
+  const { _id, email, username, books, image } = profile;
   // console.log('profile', profile)
   const [isDeleted, setIsDeleted] = useState(false);
 
   const deleteHandler = async () => {
-    // try {
-    //   const response = await fetch(
-    //     `${process.env.REACT_APP_BASE_URL}authors/delete/${_id}`,
-    //     {
-    //       method: 'DELETE',
-    //     }
-    //   );
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}authors/delete/${_id}`,
+        {
+          method: 'DELETE',
+        }
+      );
 
-    //   if (response.ok) {
-    //     setIsDeleted(true);
-    //   } else {
-    //     console.log('Error deleting profile:', response.status);
-    //   }
-    // } catch (error) {
-    //   console.log('Error:', error);
-    // }
+      if (response.ok) {
+        setIsDeleted(true);
+      } else {
+        console.log('Error deleting profile:', response.status);
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
   };
 
   if (isDeleted) {
@@ -119,7 +53,7 @@ const DeleteProfile: React.FC<ProfileProps> = ({ profile }) => {
           return (
             <div>
               <p>{book.name}</p>
-              {/* NOTE this would be the authors uploaded book section, add there whatever you want */}
+              {/* NOTE authors uploaded book section */}
               <img src={book.image} alt=""/>
               
             </div>
