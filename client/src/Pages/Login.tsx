e8da354eecdec2c15891cbe4e4cc84c0828205c2
pranabcode2023 <pranabcode2023@gmail.com>
2023-06-16 16:01:48 +0200

    changes

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -1,67 +1,53 @@
-import React, { ChangeEvent, FormEvent, useContext, useState } from 'react'
-import { AuthContext } from '../contexts/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import React, { ChangeEvent, FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
-type Props = {}
+type Props = {};
 function Login({}: Props) {
   const { login } = useContext(AuthContext);
   const [formData, setFormData] = useState<SubmitLoginData>({
     email: "",
     password: "",
   });
-  
 
- const navigate = useNavigate();  
-  
+  const navigate = useNavigate();
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-    
-  }
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {  
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    navigate('/profile');    
+    navigate("/profile");
     login(formData.email, formData.password);
-   
-  }
-   
+  };
+
   return (
     <div className="login">
-      
       <form className="form" onSubmit={handleSubmit}>
         <div className="title">Login</div>
         <div className="input-container">
-           <label>Username </label>
-          <input type='email' name='email' placeholder='email' onChange={handleChange}/>
+          <label>Username </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={handleChange}
+          />
         </div>
         <div className="input-container">
-            <label>Password </label>
-          <input type='password' name='password' placeholder='password'onChange={handleChange}/>
+          <label>Password </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={handleChange}
+          />
         </div>
         <div className="button-container">
-          <button type='submit'>Login</button>
+          <button type="submit">Login</button>
         </div>
-        
       </form>
     </div>
-
-    // <div className="login">
-    //   <form className="form" onSubmit={handleSubmit}>
-    //     <div className="title">Login</div>
-    //     <div className="input-container">
-    //       <label>Username </label>
-    //       <input type="email" placeholder='Email' onChange={handleChange} />
-        
-    //     </div>
-    //     <div className="input-container">
-    //       <label>Password </label>
-    //       <input type="password" name="pass" placeholder='Password' onChange={handleChange} />
-         
-    //     </div>
-    //     <div className="button-container">
-    //       <button type="submit" />
-    //     </div>
-    //   </form>
-    // </div>
   );
 }
 
--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,30 +1,29 @@
-import { ReactNode, createContext, useState, useEffect } from "react"
-
+import { ReactNode, createContext, useState, useEffect } from "react";
 
 interface Author {
-  _id:string
-  email?: string,
-  username: string,
-  image: string,
-  books: string[]
+  _id: string;
+  email?: string;
+  username: string;
+  image: string;
+  books: string[];
 }
 
-interface fetchResult{
-  authorToken: string,
-  verified: boolean,
-  author?: Author,
-  user?:Author
+interface fetchResult {
+  authorToken: string;
+  verified: boolean;
+  author?: Author;
+  user?: Author;
 }
 
-interface fetchFailed{
-  error: string 
+interface fetchFailed {
+  error: string;
 }
 
 interface AuthContextType {
-  author: Author | null,
-  error: Error | null,
-  login(email: string, password: string): void,
-  logout():void
+  author: Author | null;
+  error: Error | null;
+  login(email: string, password: string): void;
+  logout(): void;
 }
 
 // export const AuthContext = createContext<AuthContextType | null>(null); // not recommended
@@ -35,250 +34,104 @@ const initialAuth: AuthContextType = {
   author: null,
   error: null,
   login: () => {
-    throw new Error('login not implemented.');
+    throw new Error("login not implemented.");
   },
   logout: () => {
-    throw new Error('logout not implemented.');
-  }
+    throw new Error("logout not implemented.");
+  },
 };
 
 export const AuthContext = createContext<AuthContextType>(initialAuth);
 
-
- export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
-   const [author, setAuthor] = useState< Author | null>(null);
-   console.log("active author:",author);
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [author, setAuthor] = useState<Author | null>(null);
+  console.log("active author:", author);
   const [error, setError] = useState<Error | null>(null);
 
-  const login = async(email: string, password: string) => {
+  const login = async (email: string, password: string) => {
     // console.log({ email: email, password: password })
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-    
+
     const urlencoded = new URLSearchParams();
     urlencoded.append("email", email);
     urlencoded.append("password", password);
     const requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: myHeaders,
       body: urlencoded,
     };
     try {
-      const response = await fetch(`${process.env.REACT_APP_BASE_URL}authors/login`, requestOptions);
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}authors/login`,
+        requestOptions
+      );
       // console.log(response)
-    
+
       if (response.ok) {
-        const result = await response.json() as fetchResult
+        const result = (await response.json()) as fetchResult;
         // console.log('restult >>>>', result)
         if (result.user) {
-        setAuthor(result.user);
-        // console.log("author login function", result.user)
-        localStorage.setItem("token", result.authorToken);
-        localStorage.setItem("my name", "pranab");
+          setAuthor(result.user);
+          // console.log("author login function", result.user)
+          localStorage.setItem("token", result.authorToken);
+          localStorage.setItem("my name", "pranab");
         }
         //  console.log(result);
       } else {
-        const result = await response.json() as fetchFailed
-        alert(result.error)
-     }
-      
-     
+        const result = (await response.json()) as fetchFailed;
+        alert(result.error);
+      }
     } catch (error) {
       console.log(error);
       // setError(error); //I still have to figure out how to type the unknown fetch results
-      alert("Something went wrong - check console for error")
+      alert("Something went wrong - check console for error");
     }
+  };
 
-  }
-  
   const logout = () => {
     localStorage.removeItem("token");
     setAuthor(null);
-   }
-   
-    const checkForToken = () => {
+  };
+
+  const checkForToken = () => {
     const authorToken = localStorage.getItem("token");
     if (authorToken) {
-      console.log("There is a token")
+      console.log("There is a token");
       fetchActiveAuthor(authorToken);
     } else {
-      console.log("There is no token")
-     setAuthor(null)
+      console.log("There is no token");
+      setAuthor(null);
     }
-  }
+  };
 
-   
-   const fetchActiveAuthor = async (authorToken: string) => {
+  const fetchActiveAuthor = async (authorToken: string) => {
     const myHeaders = new Headers();
-     myHeaders.append("Authorization", `Bearer ${authorToken}`);
-     const requestOptions = {
-         method: 'GET',
-         headers: myHeaders,
-};
-     try {
-       const response = await fetch(`${process.env.REACT_APP_BASE_URL}authors/active`, requestOptions);
-       const result = await response.json();
+    myHeaders.append("Authorization", `Bearer ${authorToken}`);
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    };
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}authors/active`,
+        requestOptions
+      );
+      const result = await response.json();
       //  console.log("active author result:", result)
-       setAuthor(result);
-     } catch (error) {
-       console.log(error);
-     }
-  }
-   
-   
-   
+      setAuthor(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     checkForToken();
-  }, [])
+  }, []);
 
-return (
+  return (
     <AuthContext.Provider value={{ author, login, logout, error }}>
-      { children }
+      {children}
     </AuthContext.Provider>
-  )
-}
-
-
-
-
-// import { ReactNode, createContext, useState, useEffect } from "react"
-
-
-// interface User {
-//   email?: string,
-//   username: string,
-//   avatar: string,
-//   pets: string[]
-// }
-
-// interface fetchResult{
-//   token: string,
-//   verified: boolean,
-//   user: User
-// }
-
-// interface fetchFailed{
-//   error: string 
-// }
-
-// interface AuthContextType {
-//   user: User | null,
-//   error: Error | null,
-//   login(email: string, password: string): void,
-//   logout():void
-// }
-
-// // export const AuthContext = createContext<AuthContextType | null>(null); // not recommended
-// // export const AuthContext = createContext<AuthContextType>({} as AuthContextType); // less recommended
-// // export const AuthContext = createContext<AuthContextType>(null!); // less recommended
-
-// const initialAuth: AuthContextType = {
-//   user: null,
-//   error: null,
-//   login: () => {
-//     throw new Error('login not implemented.');
-//   },
-//   logout: () => {
-//     throw new Error('logout not implemented.');
-//   }
-// };
-
-// export const AuthContext = createContext<AuthContextType>(initialAuth);
-
-
-//  export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
-//    const [user, setUser] = useState< User | null>(null);
-//   //  console.log("active user:",user);
-//   const [error, setError] = useState<Error | null>(null);
-
-//   const login = async(email: string, password: string) => {
-//     // console.log({ email: email, password: password })
-//     const myHeaders = new Headers();
-//     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-    
-//     const urlencoded = new URLSearchParams();
-//     urlencoded.append("email", email);
-//     urlencoded.append("password", password);
-//     const requestOptions = {
-//       method: 'POST',
-//       headers: myHeaders,
-//       body: urlencoded,
-//     };
-//     try {
-//       const response = await fetch(`${process.env.REACT_APP_BASE_URL}users/login`, requestOptions);
-//       console.log(response)
-    
-//       if (response.ok) {
-//         const result = await response.json() as fetchResult
-//         if (result.user) {
-//         setUser(result.user);
-//         console.log(result.user)
-//         localStorage.setItem("token", result.token);
-//         localStorage.setItem("my name", "pranab");
-//         }
-//          console.log(result);
-//       } else {
-//         const result = await response.json() as fetchFailed
-//         alert(result.error)
-//      }
-      
-     
-//     } catch (error) {
-//       console.log(error);
-//       // setError(error); //I still have to figure out how to type the unknown fetch results
-//       alert("Something went wrong - check console for error")
-//     }
-
-//   }
-  
-//   const logout = () => {
-//     localStorage.removeItem("token");
-//     setUser(null);
-//    }
-   
-//     const checkForToken = () => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       console.log("There is a token")
-//       fetchActiveUser(token);
-//     } else {
-//       console.log("There is no token")
-//      setUser(null)
-//     }
-//   }
-
-   
-//    const fetchActiveUser = async (token: string) => {
-//     const myHeaders = new Headers();
-//      myHeaders.append("Authorization", `Bearer ${token}`);
-//      const requestOptions = {
-//          method: 'GET',
-//          headers: myHeaders,
-// };
-//      try {
-//        const response = await fetch(`${process.env.REACT_APP_BASE_URL}users/active`, requestOptions);
-//        const result = await response.json();
-//        console.log("active user result:", result)
-//        setUser(result);
-//      } catch (error) {
-//        console.log(error);
-//      }
-//   }
-   
-   
-   
-//   useEffect(() => {
-//     checkForToken();
-//   }, [])
-
-// return (
-//     <AuthContext.Provider value={{ user, login, logout, error }}>
-//       { children }
-//     </AuthContext.Provider>
-//   )
-// }
-
-
-
-
-
+  );
+};
