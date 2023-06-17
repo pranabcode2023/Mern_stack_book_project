f15f2b09ad3053ed73ea60557e9e6a4b6a069c33
pranabcode2023 <pranabcode2023@gmail.com>
2023-06-17 14:20:23 +0200

    deleteProfile updated

--- client/src/components/Profile/DeleteProfile.tsx
+++ client/src/components/Profile/DeleteProfile.tsx
@@ -1,21 +1,13 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-// interface profile: {
-//     _id: any;
-//     email: any;
-//     username: any;
-//     books: any;
-//     image: any;
-//   };
-// 
 interface ProfileProps {
-  profile:any
+  profile: any;
 }
 
 const DeleteProfile: React.FC<ProfileProps> = ({ profile }) => {
   const { _id, email, username, books, image } = profile;
-  // console.log('profile', profile)
+  // console.log("profile", profile);
   const [isDeleted, setIsDeleted] = useState(false);
 
   const deleteHandler = async () => {
@@ -23,17 +15,17 @@ const DeleteProfile: React.FC<ProfileProps> = ({ profile }) => {
       const response = await fetch(
         `${process.env.REACT_APP_BASE_URL}authors/delete/${_id}`,
         {
-          method: 'DELETE',
+          method: "DELETE",
         }
       );
 
       if (response.ok) {
         setIsDeleted(true);
       } else {
-        console.log('Error deleting profile:', response.status);
+        console.log("Error deleting profile:", response.status);
       }
     } catch (error) {
-      console.log('Error:', error);
+      console.log("Error:", error);
     }
   };
 
@@ -45,35 +37,28 @@ const DeleteProfile: React.FC<ProfileProps> = ({ profile }) => {
     <div className="bookCard">
       <div className="container">
         <img src={profile?.image} alt="" />
-        <h3>User Name:  {profile?.username}</h3>
-        <h3>Email:  {profile?.email}</h3>
-      <div className="container">
-        <Link to={`/profile/${_id}`}>
-          <button>Update</button>
-        </Link>
-      </div>
+        <h3>User Name: {profile?.username}</h3>
+        <h3>Email: {profile?.email}</h3>
+        <div className="container">
+          <Link to={`/profile/${_id}`}>
+            <button>Update</button>
+          </Link>
+        </div>
         <button onClick={deleteHandler}>Delete </button>
-          <h1>Uploaded Books</h1>
-        {profile.books && profile.books.map((book:any) => {
-          return (
-            <div>
-              <p>{book.name}</p>
-              {/* NOTE authors uploaded book section */}
-              <img src={book.image} alt=""/>
-            </div>
-          )
-        })}
-     </div>
+        <h1>Uploaded Books</h1>
+        {profile.books &&
+          profile.books.map((book: any) => {
+            return (
+              <div>
+                <p>{book.name}</p>
+                {/* NOTE authors uploaded book section */}
+                <img src={book.image} alt="" />
+              </div>
+            );
+          })}
+      </div>
     </div>
-
-
   );
 };
 
 export default DeleteProfile;
-
-
-
-
-
-
